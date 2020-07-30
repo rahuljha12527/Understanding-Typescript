7405bda96b72e95b59f9c50693565dd6ef8c50a1
rahuljha661 <rahuljha12527@gmail.com>
2020-07-30 15:29:24 +0530

    added module

--- public/app.js
+++ public/app.js
@@ -1,31 +1,12 @@
-"use strict";
 // classes
-var Invoice = /** @class */ (function () {
-    //   readonly client: string;
-    //   private details: string;
-    //   public amount: number;
-    //   constructor(c: string, d: string, a: number) {
-    //     this.client = c;
-    //     this.details = d;
-    //     this.amount = a;
-    //   }
-    function Invoice(client, details, amount) {
-        this.client = client;
-        this.details = details;
-        this.amount = amount;
-    }
-    Invoice.prototype.format = function () {
-        return this.client + " owes " + this.amount + " for " + this.details;
-    };
-    return Invoice;
-}());
-var invOne = new Invoice("mario", "work on the mario website", 250);
-var invTwo = new Invoice("luigi", "work on the luigi website", 300);
+import { Invoice } from './classes/invoice.js';
+const invOne = new Invoice("mario", "work on the mario website", 250);
+const invTwo = new Invoice("luigi", "work on the luigi website", 300);
 console.log(invOne, invTwo);
-var invoice = [];
+let invoice = [];
 invoice.push(invOne);
 invoice.push(invTwo);
-invoice.forEach(function (inv) {
+invoice.forEach((inv) => {
     console.log("trtr", inv.client, inv.amount, inv.format());
 });
 //invOne.client = "yoshi";
@@ -36,14 +17,14 @@ console.log(invOne, invTwo);
 // console.log(anchor);
 // }
 // const form=document.querySelector('form')!;
-var form = document.querySelector(".new-item-form");
+const form = document.querySelector(".new-item-form");
 console.log(form.children);
 // inputs
-var type = document.querySelector("#type");
-var tofrom = document.querySelector("#tofrom");
-var details = document.querySelector("#details");
-var amount = document.querySelector("#amount");
-form.addEventListener("submit", function (e) {
+const type = document.querySelector("#type");
+const tofrom = document.querySelector("#tofrom");
+const details = document.querySelector("#details");
+const amount = document.querySelector("#amount");
+form.addEventListener("submit", (e) => {
     e.preventDefault();
     console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,5 @@
 // classes
-class Invoice {
-//   readonly client: string;
-//   private details: string;
-//   public amount: number;
-
-//   constructor(c: string, d: string, a: number) {
-//     this.client = c;
-//     this.details = d;
-//     this.amount = a;
-//   }
-
-constructor(
-    readonly client:string,
-    private details:string,
-    public amount:number,
-){
-
-}
-
-  format() {
-    return `${this.client} owes ${this.amount} for ${this.details}`;
-  }
-}
+import {Invoice} from './classes/invoice.js';
 
 const invOne = new Invoice("mario", "work on the mario website", 250);
 const invTwo = new Invoice("luigi", "work on the luigi website", 300);
