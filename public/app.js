a82363218a6bdfce1f5e5d80cd8d1bb00faa6c93
rahuljha661 <rahuljha12527@gmail.com>
2020-07-30 15:10:58 +0530

    public private protected read only

--- public/app.js
+++ public/app.js
@@ -1,39 +1,49 @@
 "use strict";
 // classes
 var Invoice = /** @class */ (function () {
-    function Invoice(c, d, a) {
-        this.client = c;
-        this.details = d;
-        this.amount = a;
+    //   readonly client: string;
+    //   private details: string;
+    //   public amount: number;
+    //   constructor(c: string, d: string, a: number) {
+    //     this.client = c;
+    //     this.details = d;
+    //     this.amount = a;
+    //   }
+    function Invoice(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
     }
     Invoice.prototype.format = function () {
         return this.client + " owes " + this.amount + " for " + this.details;
     };
     return Invoice;
 }());
-var invOne = new Invoice('mario', 'work on the mario website', 250);
-var invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+var invOne = new Invoice("mario", "work on the mario website", 250);
+var invTwo = new Invoice("luigi", "work on the luigi website", 300);
 console.log(invOne, invTwo);
 var invoice = [];
 invoice.push(invOne);
 invoice.push(invTwo);
-console.log(invoice);
-invOne.client = 'yoshi';
+invoice.forEach(function (inv) {
+    console.log("trtr", inv.client, inv.amount, inv.format());
+});
+//invOne.client = "yoshi";
 invTwo.amount = 400;
 console.log(invOne, invTwo);
 // const anchor=document.querySelector('a')!;
 // if(anchor){
-// console.log(anchor); 
+// console.log(anchor);
 // }
 // const form=document.querySelector('form')!;
-var form = document.querySelector('.new-item-form');
+var form = document.querySelector(".new-item-form");
 console.log(form.children);
 // inputs
-var type = document.querySelector('#type');
-var tofrom = document.querySelector('#tofrom');
-var details = document.querySelector('#details');
-var amount = document.querySelector('#amount');
-form.addEventListener('submit', function (e) {
+var type = document.querySelector("#type");
+var tofrom = document.querySelector("#tofrom");
+var details = document.querySelector("#details");
+var amount = document.querySelector("#amount");
+form.addEventListener("submit", function (e) {
     e.preventDefault();
     console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,74 +1,66 @@
 // classes
-class Invoice{
-   client:string;
-   details:string;
-   amount:number;
-   
-   constructor(c:string,d:string,a:number){
-       this.client=c;
-       this.details=d;
-       this.amount=a;
-   }
+class Invoice {
+//   readonly client: string;
+//   private details: string;
+//   public amount: number;
 
-   format(){
-       return `${this.client} owes ${this.amount} for ${this.details}`;
-   }
+//   constructor(c: string, d: string, a: number) {
+//     this.client = c;
+//     this.details = d;
+//     this.amount = a;
+//   }
+
+constructor(
+    readonly client:string,
+    private details:string,
+    public amount:number,
+){
+
 }
 
-const invOne=new Invoice('mario','work on the mario website',250);
-const invTwo=new Invoice('luigi','work on the luigi website',300);
+  format() {
+    return `${this.client} owes ${this.amount} for ${this.details}`;
+  }
+}
 
+const invOne = new Invoice("mario", "work on the mario website", 250);
+const invTwo = new Invoice("luigi", "work on the luigi website", 300);
 
-console.log(invOne,invTwo);
+console.log(invOne, invTwo);
 
-let invoice:Invoice[]=[];
+let invoice: Invoice[] = [];
 invoice.push(invOne);
 invoice.push(invTwo);
 
-console.log(invoice);
+invoice.forEach((inv) => {
+  console.log("trtr", inv.client, inv.amount, inv.format());
+});
 
-invOne.client='yoshi';
-invTwo.amount=400;
+//invOne.client = "yoshi";
+invTwo.amount = 400;
 
-console.log(invOne,invTwo);
-
-
-
-
+console.log(invOne, invTwo);
 
 // const anchor=document.querySelector('a')!;
 
 // if(anchor){
-// console.log(anchor); 
-
+// console.log(anchor);
 
 // }
 
 // const form=document.querySelector('form')!;
 
-const form=document.querySelector('.new-item-form') as HTMLFormElement;
+const form = document.querySelector(".new-item-form") as HTMLFormElement;
 console.log(form.children);
 
 // inputs
-const type=document.querySelector('#type') as HTMLSelectElement;
-const tofrom=document.querySelector('#tofrom') as HTMLInputElement;
-const details=document.querySelector('#details') as HTMLInputElement;
-const amount=document.querySelector('#amount') as HTMLInputElement;
+const type = document.querySelector("#type") as HTMLSelectElement;
+const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
+const details = document.querySelector("#details") as HTMLInputElement;
+const amount = document.querySelector("#amount") as HTMLInputElement;
 
+form.addEventListener("submit", (e: Event) => {
+  e.preventDefault();
 
-form.addEventListener('submit',(e:Event)=>{
-    e.preventDefault();
-
-    console.log(
-        type.value,
-        tofrom.value,
-        details.value,
-        amount.valueAsNumber
-    )
-})
-
-
-
-
-
-
+  console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+});
