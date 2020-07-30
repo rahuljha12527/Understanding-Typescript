a6b671fcadb06c863f0b7a363f911e3ef1556479
rahuljha661 <rahuljha12527@gmail.com>
2020-07-30 14:56:11 +0530

    understand how to create a class

--- public/app.js
+++ public/app.js
@@ -1,4 +1,26 @@
 "use strict";
+// classes
+var Invoice = /** @class */ (function () {
+    function Invoice(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    Invoice.prototype.format = function () {
+        return this.client + " owes " + this.amount + " for " + this.details;
+    };
+    return Invoice;
+}());
+var invOne = new Invoice('mario', 'work on the mario website', 250);
+var invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+console.log(invOne, invTwo);
+var invoice = [];
+invoice.push(invOne);
+invoice.push(invTwo);
+console.log(invoice);
+invOne.client = 'yoshi';
+invTwo.amount = 400;
+console.log(invOne, invTwo);
 // const anchor=document.querySelector('a')!;
 // if(anchor){
 // console.log(anchor); 
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,41 @@
+// classes
+class Invoice{
+   client:string;
+   details:string;
+   amount:number;
+   
+   constructor(c:string,d:string,a:number){
+       this.client=c;
+       this.details=d;
+       this.amount=a;
+   }
+
+   format(){
+       return `${this.client} owes ${this.amount} for ${this.details}`;
+   }
+}
+
+const invOne=new Invoice('mario','work on the mario website',250);
+const invTwo=new Invoice('luigi','work on the luigi website',300);
+
+
+console.log(invOne,invTwo);
+
+let invoice:Invoice[]=[];
+invoice.push(invOne);
+invoice.push(invTwo);
+
+console.log(invoice);
+
+invOne.client='yoshi';
+invTwo.amount=400;
+
+console.log(invOne,invTwo);
+
+
+
+
+
 // const anchor=document.querySelector('a')!;
 
 // if(anchor){
